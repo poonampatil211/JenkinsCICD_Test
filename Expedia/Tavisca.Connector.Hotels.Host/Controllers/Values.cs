32180a73f4336a0665219a1c5d6075233d114439
poonampatilExt <poonam.patil@Extentia.com>
2017-08-08 14:58:38 +0530

    sdfs fs fs fas f as

--- Expedia/Tavisca.Connector.Hotels.Host/Controllers/Values.cs
+++ Expedia/Tavisca.Connector.Hotels.Host/Controllers/Values.cs
@@ -15,7 +15,7 @@ namespace Travel.Connectors.Hotel.Controllers
         public IEnumerable<string> Get(string test)
         {
             //testData();
-            return new string[] { "value1", "val e"};
+            return new string[] { "value1", "valsdf dfgd "};
         }
 
         // GET api/values/5
